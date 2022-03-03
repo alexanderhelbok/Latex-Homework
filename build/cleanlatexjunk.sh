d1fcfc5d75c1e529944eeea806dda5068a522867
Alexander Helbok <alexander.helbok@hotmail.com>
2022-03-03 08:56:16 +0100

    Committed by TeXstudio

--- build/cleanlatexjunk.sh
+++
@@ -1,2 +0,0 @@
-cp '/home/taco/Documents/Latex HÜ/build/'$1.pdf '/home/taco/Documents/Latex HÜ/'$1.pdf
-cp '/home/taco/Documents/Latex HÜ/build/'$1.pdf '/home/taco/Documents/Latex HÜ/PDF/'$1.pdf
--- build/git.sh
+++
@@ -1,3 +0,0 @@
-git add -A
-git commit -a -m "New"
-git push -u origin main
