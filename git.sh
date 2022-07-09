a5e4ddd29fdd279004679d088729737a9d2c47c8
Alexander Helbok <alexander.helbok@hotmail.com>
2022-07-09 23:27:29 +0200

    Committed by TeXstudio

--- git.sh
+++ git.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 shopt -s extglob
 
-path=${1%%*([0-9a-zA-Z-._])}
-path=${path//[ ]/' '}
+path=${1%%*([0-9a-zA-Z-._])}  # remove filename
+path=${path//[ ]/' '}         # enclose space in quotes
 mode=$2
 
-echo $path
-echo $2
+# Debug
+# echo $path
+# echo $2
 
-if [ $mode = "pull" ]
-then
+if [ $mode = "pull" ]; then
     git -C "$path" pull
-else
+elif [ $mode = "push" ]; then
     git -C "$path" commit -a -m "Committed by TeXstudio"
     git -C "$path" push -u origin "main"
+elif [ $mode = "stash" ]; then
+    git -C "$path" stash
 fi
